e20a0657bc8435a2ffa8966a7f78846164df9806
Ishwar <ishwarp716@gmail.com>
2022-07-14 12:58:54 +0530

    a

--- db.js
+++ db.js
@@ -6,19 +6,19 @@ const poolConfig = process.env.DATABASE_URL?{
     ssl:{
         rejectUnauthorized: false
     }
+}:{
+    user: 'postgres',
+    password: 'I@1234',
+    database: 'foodproduct',
+    host:'',
+    port: 5432, 
+};
 // }:{
-//     user: 'postgres',
-//     password: 'I@1234',
-//     database: 'foodproduct',
+//     user: 'ipxxjnbsersypp',
+//     password: 'I@ec1e090b9789c37d652ba00518d9e26b04f310dfc057de67870cf788694f9be8',
+//     database: 'dfu67as0cohnsc',
 //     host:'ec2-44-195-169-163.compute-1.amazonaws.com',
 //     port: 5432, 
 // };
-}:{
-    user: 'ipxxjnbsersypp',
-    password: 'I@ec1e090b9789c37d652ba00518d9e26b04f310dfc057de67870cf788694f9be8',
-    database: 'dfu67as0cohnsc',
-    host:'ec2-44-195-169-163.compute-1.amazonaws.com',
-    port: 5432, 
-};
 const pool = new Pool(poolConfig);
 export default pool;
